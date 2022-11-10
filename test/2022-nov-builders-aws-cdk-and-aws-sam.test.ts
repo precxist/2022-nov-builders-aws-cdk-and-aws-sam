31034ddce07bbf79d25e2e1575de68a543c2dbc0
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-2.compute.internal>
2022-11-10 15:20:38 +0000

    Initial commit

---
+++ test/2022-nov-builders-aws-cdk-and-aws-sam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 2022NovBuildersAwsCdkAndAwsSam from '../lib/2022-nov-builders-aws-cdk-and-aws-sam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/2022-nov-builders-aws-cdk-and-aws-sam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 2022NovBuildersAwsCdkAndAwsSam.2022NovBuildersAwsCdkAndAwsSamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
